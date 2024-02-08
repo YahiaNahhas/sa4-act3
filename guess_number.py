638e22c4419cdb6ad9eb4969bcc2e68468e76083
Yahia Nahhas <yahianahhas15@gmail.com>
2024-02-07 19:06:46 -0600

    Refactored Number Guess game to multiple user inputs

--- guess_number.py
+++ guess_number.py
@@ -1,9 +1,17 @@
-number = 10 
+number = '10' 
 
 print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
+
+guess = str(input("What number am I thinking of? "))
+ 
+while guess != number and guess != 'q':
+    print("Incorret guess. Try Again!")    
+
+    guess = str(input("What number am I thinking of? "))
 
 if guess == number:
-    print("Congratulations! you guessed the right number. ")
-else:
-    print(f"Sorry! the number was {number}.")
+     print("Congratulations! you guessed the right number. ")
+elif guess == 'q':
+     print(f"Sorry! the number was {number}.")
+  
+    
