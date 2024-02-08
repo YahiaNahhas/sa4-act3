41e3b479079f4f8fbd36a57acb21981d84fa314a
Yahia Nahhas <yahianahhas15@gmail.com>
2024-02-07 19:28:01 -0600

    Refactored Number Guess Game with Hints

--- guess_number.py
+++ guess_number.py
@@ -1,11 +1,14 @@
-number = '10' 
+number = '10'
 
 print("I'm thinking of a number...")
 
 guess = str(input("What number am I thinking of? "))
  
 while guess != number and guess != 'q':
-    print("Incorret guess. Try Again!")    
+    if int(guess) > int(number):
+         print("Incorret guess. You Guessed Too High!")    
+    elif int(guess) < int(number):
+         print("Incorrect Guess! You Guessed Too Low!")
 
     guess = str(input("What number am I thinking of? "))
 
@@ -13,5 +16,4 @@ if guess == number:
      print("Congratulations! you guessed the right number. ")
 elif guess == 'q':
      print(f"Sorry! the number was {number}.")
-  
-    
+     
